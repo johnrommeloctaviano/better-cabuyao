6862a0fa0fba766371a1fa34c533ec9fb6586313
johnrommeloctaviano <johnrommel.octaviano@gmail.com>
2026-04-20 06:22:00 +0800

    feat: Add brief history timeline

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Hero from '../components/sections/Hero';
 import ServicesSection from '../components/home/ServicesSection';
+import HistoryTimeline from '../components/home/HistoryTimeline';
 import SEO from '../components/SEO';
 
 const Home: React.FC = () => {
@@ -13,6 +14,7 @@ const Home: React.FC = () => {
       <main className="flex-grow">
         <Hero />
         <ServicesSection />
+        <HistoryTimeline />
       </main>
     </>
   );
